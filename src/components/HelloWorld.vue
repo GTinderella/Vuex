668d5c757c230779d680e2a6b8ea9a069851e90f
Tinderella <1260448292@qq.com>
2019-12-10 10:04:48 +0800

    项目初始化

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>vuex-shopCart</h1>
+        <div class="shop-listbox">
+            <shop-list/>
+        </div>
+        <h2>已选商品</h2>
+        <div class="shop-cartbox">
+            <shop-cart/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+@keyframes myfirst {
+  from {
+    width: 100px;
+    background: red;
+  }
+  to {
+    width: 200px;
+    background: yellow;
+  }
+}
+h1 {
+  animation: myfirst 5s;
+  width: 100px;
+  color: rgb(75, 131, 75);
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+</style>
